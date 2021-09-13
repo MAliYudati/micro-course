f9afcf240928db167f21cc1589cf8af3f851e517
MAliYudati <aliyudhati12@gmail.com>
2021-09-13 11:29:20 +0700

    first init

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('mentors', 'App\Http\Controllers\MentorController@index');
+Route::get('mentors/{id}', 'App\Http\Controllers\MentorController@show');
+Route::post('mentors', 'App\Http\Controllers\MentorController@create');
+Route::put('mentors/{id}', 'App\Http\Controllers\MentorController@update');
+Route::delete('mentors/{id}', 'App\Http\Controllers\MentorController@destroy');
+
+Route::post('courses', 'App\Http\Controllers\CourseController@create');
+Route::put('courses/{id}', 'App\Http\Controllers\CourseController@update');
+Route::get('courses/{id}', 'App\Http\Controllers\CourseController@show');
+Route::get('courses', 'App\Http\Controllers\CourseController@index');
+Route::delete('courses/{id}', 'App\Http\Controllers\CourseController@destroy');
+
+Route::post('chapters', 'App\Http\Controllers\ChapterController@create');
+Route::put('chapters/{id}', 'App\Http\Controllers\ChapterController@update');
+Route::get('chapters', 'App\Http\Controllers\ChapterController@index');
+Route::get('chapters/{id}', 'App\Http\Controllers\ChapterController@show');
+Route::delete('chapters/{id}', 'App\Http\Controllers\ChapterController@destroy');
+
+Route::post('lessons', 'App\Http\Controllers\LessonController@create');
+Route::put('lessons/{id}', 'App\Http\Controllers\LessonController@update');
+Route::get('lessons', 'App\Http\Controllers\LessonController@index');
+Route::get('lessons/{id}', 'App\Http\Controllers\LessonController@show');
+Route::delete('lessons/{id}', 'App\Http\Controllers\LessonController@destroy');
+
+Route::post('image-courses', 'App\Http\Controllers\ImageCourseController@create');
+Route::delete('image-courses/{id}', 'App\Http\Controllers\ImageCourseController@destroy');
+
+Route::post('my-courses', 'App\Http\Controllers\MyCourseController@create');
+Route::get('my-courses', 'App\Http\Controllers\MyCourseController@index');
+Route::post('my-courses/premium', 'App\Http\Controllers\MyCourseController@createPremiumAccess');
+
+Route::post('reviews', 'App\Http\Controllers\ReviewController@create');
+Route::put('reviews/{id}', 'App\Http\Controllers\ReviewController@update');
+Route::delete('reviews/{id}', 'App\Http\Controllers\ReviewController@destroy');
